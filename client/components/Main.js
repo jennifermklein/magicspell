ff2b2cc32f44bb80ba6882d6ca6ce249a42e7059
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-04-28 06:39:36 -0400

    move helper functions to separate file

--- client/components/Main.js
+++ client/components/Main.js
@@ -2,40 +2,7 @@ import React, { Component } from "react";
 import { v4 as uuid } from "uuid";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { LetterList } from "./LetterList";
-
-// a little function to help us with reordering the result
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
-/**
- * Moves an item from one list to another list.
- */
-const copy = (source, destination, droppableSource, droppableDestination) => {
-  const sourceClone = Array.from(source);
-  const destClone = Array.from(destination);
-  const item = sourceClone[droppableSource.index];
-
-  destClone.splice(droppableDestination.index, 0, { ...item, id: uuid() });
-  return destClone;
-};
-
-const move = (source, destination, droppableSource, droppableDestination) => {
-  const sourceClone = Array.from(source);
-  const destClone = Array.from(destination);
-  const [removed] = sourceClone.splice(droppableSource.index, 1);
-
-  destClone.splice(droppableDestination.index, 0, removed);
-
-  const result = {};
-  result[droppableSource.droppableId] = sourceClone;
-  result[droppableDestination.droppableId] = destClone;
-
-  return result;
-};
+import { reorder, copy, move } from "../helpers/ordering";
 
 const ITEMS = [
   {
@@ -146,8 +113,8 @@ const ITEMS = [
 
 class Main extends Component {
   state = {
-    [uuid()]: ["A"],
-    [uuid()]: ["B"],
+    [uuid()]: [],
+    [uuid()]: [],
   };
   onDragEnd = (result) => {
     const { source, destination } = result;
@@ -197,7 +164,12 @@ class Main extends Component {
   render() {
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
-        <Droppable droppableId="ITEMS" isDropDisabled={true}>
+        <Droppable
+          droppableId="ITEMS"
+          isDropDisabled={true}
+          direction="horizontal"
+          isCombineEnabled={false}
+        >
           {(provided, snapshot) => (
             <div className="list-container">
               <div
--- client/helpers/ordering.js
+++ client/helpers/ordering.js
@@ -1,5 +1,7 @@
-// function to reorder result
-const reorder = (list, startIndex, endIndex) => {
+import { v4 as uuid } from "uuid";
+
+// a little function to help us with reordering the result
+export const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
@@ -7,52 +9,35 @@ const reorder = (list, startIndex, endIndex) => {
   return result;
 };
 
-export default (letters, source, destination) => {
-  console.log("letters", letters, "source", source, "dest", destination);
-  const current = [...letters[source.droppableId]];
-  const next = [...letters[destination.droppableId]];
-  const target = current[source.index];
+export const copy = (
+  source,
+  destination,
+  droppableSource,
+  droppableDestination
+) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const item = sourceClone[droppableSource.index];
 
-  // moving to same list
-  if (source.droppableId === destination.droppableId) {
-    const reordered = reorder(current, source.index, destination.index);
-    return { ...letters, [source.droppableId]: reordered };
-  }
-
-  // moving to different list
-  // remove from original
-  current.splice(source.index, 1);
-  // insert into next
-  next.splice(destination.index, 0, target);
-
-  return {
-    ...letters,
-    [source.droppableId]: current,
-    [destination.droppableId]: next,
-  };
+  destClone.splice(droppableDestination.index, 0, { ...item, id: uuid() });
+  return destClone;
 };
 
-// const copy = (source, destination, droppableSource, droppableDestination) => {
-//   console.log("==> dest", destination);
+export const move = (
+  source,
+  destination,
+  droppableSource,
+  droppableDestination
+) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
 
-//   const sourceClone = Array.from(source);
-//   const destClone = Array.from(destination);
-//   const item = sourceClone[droppableSource.index];
+  destClone.splice(droppableDestination.index, 0, removed);
 
-//   destClone.splice(droppableDestination.index, 0, { ...item, id: uuid() });
-//   return destClone;
-// };
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
 
-// const move = (source, destination, droppableSource, droppableDestination) => {
-//   const sourceClone = Array.from(source);
-//   const destClone = Array.from(destination);
-//   const [removed] = sourceClone.splice(droppableSource.index, 1);
-
-//   destClone.splice(droppableDestination.index, 0, removed);
-
-//   const result = {};
-//   result[droppableSource.droppableId] = sourceClone;
-//   result[droppableDestination.droppableId] = destClone;
-
-//   return result;
-// };
+  return result;
+};

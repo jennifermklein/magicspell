f306fd1674e8786e0dca2bbef4a6a6eb00f427cc
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-04-29 17:33:01 -0400

    remove comments

--- client/Routes.js
+++ client/Routes.js
@@ -6,9 +6,6 @@ import Home from "./components/Home";
 import { me } from "./store";
 import Main from "./components/Main";
 
-/**
- * COMPONENT
- */
 class Routes extends Component {
   componentDidMount() {
     this.props.loadInitialData();
@@ -39,9 +36,6 @@ class Routes extends Component {
   }
 }
 
-/**
- * CONTAINER
- */
 const mapState = (state) => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.

fc524f9e3231c527d0d897201a8ab409e071f0c3
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-04-29 10:12:29 -0400

    button styles

--- client/components/AddList.js
+++ client/components/AddList.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Button } from "react-rainbow-components";
+import { ButtonIcon } from "react-rainbow-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export const AddList = ({ addList }) => {
   return (
-    <Button
-      className="rainbow-m-around_medium"
-      label="+"
-      variant="success"
-      onClick={addList}
-      size="small"
-    />
+    <button id="add-list" onClick={addList}>
+      <FontAwesomeIcon icon={faPlus} />
+    </button>
   );
 };
--- client/components/SayAgain.js
+++ client/components/SayAgain.js
@@ -2,27 +2,13 @@ import React from "react";
 import { Button } from "react-rainbow-components";
 import { ButtonIcon } from "react-rainbow-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-regular-svg-icons";
+// import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 export const SayAgain = ({ sayAgain, listId }) => {
   return (
-    <div className="rainbow-p-right_large">
-      <ButtonIcon
-        variant="neutral"
-        icon={
-          <FontAwesomeIcon icon={faStar} onClick={(e) => sayAgain(e, listId)} />
-        }
-      />
-    </div>
+    <button id="play-again" onClick={(e) => sayAgain(e, listId)}>
+      <FontAwesomeIcon icon={faPlay} />
+    </button>
   );
 };
-
-{
-  /* <Button
-className="rainbow-m-around_medium"
-label="SAY"
-variant="success"
-onClick={(e) => sayAgain(e, listId)}
-size="small"
-/> */
-}
